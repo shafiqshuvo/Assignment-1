f5194110b2bef119f86cf25c0544a19c9e6617de
shafiq <shafiq.shuvo.nstu.ice@gmail.com>
2019-09-11 17:54:43 +0600

    2 console app commited

---
+++ First/First/Program.cs
@@ -0,0 +1,27 @@
+﻿//various datatypes
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Shafiq";
+            Console.WriteLine(name);
+            double age = 24.5;
+            Console.WriteLine(age);
+            int income = 10000;
+            Console.WriteLine(income);
+            Boolean status = true;
+            Console.WriteLine(status);
+            Console.ReadKey();
+        }
+    }
+}
